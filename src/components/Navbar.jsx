ef23ba02ec4fdf3499397e6ed3ce4d98c0baed61
Azhar <eng.azharkhan@gmail.com>
2022-04-10 08:14:01 +0500

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,22 +36,22 @@ const Navbar = () => {
       <div className='hidden lg:flex flex-col fixed top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a href='' className='flex justify-between items-center w-full text-gray-300'>
+            <a href='#' className='flex justify-between items-center w-full text-gray-300'>
               Linkedin <FaLinkedin size={30}/>
               </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a href='' className='flex justify-between items-center w-full text-gray-300'>
+            <a href='#' className='flex justify-between items-center w-full text-gray-300'>
               Github <FaGithub size={30}/>
               </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a href='' className='flex justify-between items-center w-full text-gray-300'>
+            <a href='#' className='flex justify-between items-center w-full text-gray-300'>
               Email <HiOutlineMail size={30}/>
               </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a href='' className='flex justify-between items-center w-full text-gray-300'>
+            <a href='#' className='flex justify-between items-center w-full text-gray-300'>
               Resume <BsFillPersonLinesFill size={30}/>
               </a>
           </li>
