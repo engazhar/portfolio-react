a2473e02433f1afcde8e2fa04db1e0d2996cf9b6
Azhar <eng.azharkhan@gmail.com>
2022-04-11 07:25:06 +0500

    fevicon andlogo are updated

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div id="root">
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Azhar Nazir - Portfolio</title>
+          <link rel="canonical" href="https://magenta-dodol-530e69.netlify.app" />
+      </Helmet>
       <Navbar/>
       <Home/>
       <About/>
