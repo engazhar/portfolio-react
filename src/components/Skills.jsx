f11d42fd9f9921940c1e461ac84e110e9afbdfa1
Azhar <eng.azharkhan@gmail.com>
2023-05-01 19:36:37 +0500

    screen height

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className='w-full h-full bg-[#0a192f] text-gray-300'>
+    <div name="skills" className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Skills</p>
